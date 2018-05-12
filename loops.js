5b9febd931dabf4cb1c92cd176344cd6e6d039f5
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-12 02:27:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,7 +19,10 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-    function maybeTrue() {
+  function maybeTrue() {
     return Math.random() >= 0.5
+  }
+  do {
+    array.length > 0 && maybeTrue()
   }
 }
